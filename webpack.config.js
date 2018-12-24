e00788d526ded5e256d3372f85ab239f744aef5f
wps13 <willanepaiva@hotmail.com>
2018-12-24 00:16:30 -0300

    remove comment in webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -31,14 +31,7 @@ module.exports = {
                         loader: "style-loader"
                     },
                     {
-                        loader: "css-loader",
-                        /* options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true
-                        } */
+                        loader: "css-loader"
                     }
                 ]
             }
