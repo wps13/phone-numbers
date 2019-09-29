2921e617a34c3b9d4d8b82dfe0e97b6de5d08b1b
Willane Paiva <willanepaiva@hotmail.com>
2019-09-29 19:47:31 -0300

    Add stories for number card and data input

---
+++ src/stories/components.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import NumberCard from "../components/NumberCard";
+import DataInput from "../components/DataInput";
+
+storiesOf("Components", module)
+  .add("Number card", () => <NumberCard number={555000013} cost={1.13} />)
+  .add("Input Page", () => (
+    <DataInput
+      text="Page"
+      change={e => {
+        console.log(e.target.value);
+      }}
+    />
+  ))
+  .add("Input Per Page", () => (
+    <DataInput
+      text="Numbers Per Page"
+      change={e => {
+        console.log(e.target.value);
+      }}
+    />
+  ));
