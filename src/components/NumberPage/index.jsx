7d16613dc326cd67edfad2ceaf74d921e56132ce
Willane Paiva <willanepaiva@hotmail.com>
2019-10-10 21:40:11 -0300

    Add initial number page

---
+++ src/components/NumberPage/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import NumberCard from "../NumberCard";
+import Pagination from "../Pagination";
+
+import "./style.css";
+
+const NumberPage = ({ data }) => {
+  return (
+    <div className="number-page">
+      <h1>Avaliable lines</h1>
+      {data.map(line => (
+        <NumberCard number={line.number} cost={line.cost} key={line.cost} />
+      ))}
+      <Pagination length={data.length} />
+    </div>
+  );
+};
+
+export default NumberPage;
