3dd102122abdbba8886b8e09810aedb9b29723d4
Willane Paiva <willanepaiva@hotmail.com>
2019-10-10 21:41:45 -0300

    Add fake data to build page

---
+++ src/utils/fakeData.js
@@ -0,0 +1,6 @@
+export let fakeNumbers = [
+  { number: 555000009, cost: "1.09" },
+  { number: 555000010, cost: "1.10" },
+  { number: 555000011, cost: "1.11" },
+  { number: 555000012, cost: "1.12" }
+];
