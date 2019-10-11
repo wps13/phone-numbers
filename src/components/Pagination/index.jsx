04789f0d28333c1195a8808735a126a97f1003ad
Willane Paiva <willanepaiva@hotmail.com>
2019-10-10 21:16:03 -0300

    Update pagination to be dynamic

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -3,10 +3,16 @@ import PaginationItem from "../PaginationItem";
 
 import "./style.css";
 
-const Pagination = () => {
+const Pagination = ({ length }) => {
+  let pagItems = [];
+  for (let i = 1; i <= parseInt(length); i++) {
+    pagItems.push(<PaginationItem text={i} type="number" />);
+  }
+
   return (
     <div className="pagination">
       <PaginationItem text="Previous" type="text" />
+      {pagItems.map(item => item)}
       <PaginationItem text="Next" type="text" />
     </div>
   );
