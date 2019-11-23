c3d1bc738e365ec9e507ce11895a0a1a3b567fdd
Willane Paiva <willanepaiva@hotmail.com>
2019-11-23 10:32:47 -0300

    Move store config from index to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Display from './containers/display'
+import Display from "./containers/display";
+
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+
+import { reducer } from "./store/redux";
+import { watchSaga } from "./store/sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(watchSaga);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-         <Display />
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <Display />
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import { Provider } from 'react-redux'
-
-import { reducer } from './store/redux'
-import { watchSaga } from './store/sagas'
-
-const sagaMiddleware = createSagaMiddleware()
-
-const store = createStore(reducer,
-    applyMiddleware(watchSaga))
-
-
-sagaMiddleware.run(watchSaga)
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
