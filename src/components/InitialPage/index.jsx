879ea4668e52c5d79a8c1c9ff080e6d1f48a235d
Willane Paiva <willanepaiva@hotmail.com>
2019-11-23 10:34:11 -0300

    Add function to request data and modify input name

--- src/components/InitialPage/index.jsx
+++ src/components/InitialPage/index.jsx
@@ -5,15 +5,21 @@ import GetNumbersButton from "../GetNumbersButton";
 
 import "./style.css";
 
-const InitialPage = ({ change }) => {
+const InitialPage = ({ change, requestNumbers }) => {
   return (
     <div className="initial-page">
       <h1>Phone Numbers</h1>
       <div className="initial-page__inputs">
-        <DataInput text="Page to be Viewed" change={e => change(e)} />
-        <DataInput text="Amount of pages" change={e => change(e)} />
+        <DataInput
+          text="Page to be Viewed"
+          change={e => change("page", e.target.value)}
+        />
+        <DataInput
+          text="Amount of pages"
+          change={e => change("amount", e.target.value)}
+        />
       </div>
-      <GetNumbersButton />
+      <GetNumbersButton action={requestNumbers} />
     </div>
   );
 };
