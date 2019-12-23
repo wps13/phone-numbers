d037690d71a2858c9f6a92c3cb9cb2782e3d242c
Willane Paiva <willanepaiva@hotmail.com>
2019-12-23 12:52:31 -0300

    add amount of page and modify initial state

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,46 +1,49 @@
 //inital state
 const initialState = {
-    loading:false,
-    generatedData:null,
-    error:null
-}
+  loading: false,
+  generatedData: [],
+  error: " ",
+  totalPages: 0
+};
 
 //reducer
 export function reducer(state = initialState, action) {
-    switch (action.type) {
-        case "GET_NUMBERS_REQUEST":
-            return {
-                ...state,
-                loading: true
-            }
-        case "GET_NUMBERS_SUCCESS":
-            return {
-                ...state,
-                generatedData: action.payload.data,
-                loading:false
-            }
-        case "GET_NUMBERS_FAILURE":
-            return {
-                ...state,
-                error: action.payload.error,
-                loading:false
-            }
-        default:
-            return state
-       }
+  switch (action.type) {
+    case "GET_NUMBERS_REQUEST":
+      return {
+        ...state,
+        loading: true
+      };
+    case "GET_NUMBERS_SUCCESS":
+      return {
+        ...state,
+        generatedData: action.payload.data,
+        totalPages: action.payload.totalPages,
+        loading: false
+      };
+    case "GET_NUMBERS_FAILURE":
+      return {
+        ...state,
+        error: action.payload.error,
+        loading: false
+      };
+    default:
+      return state;
+  }
 }
 
 //actions
 export const creators = {
-    getNumbersRequest : () => ({
-        type: "GET_NUMBERS_REQUEST"
-    }),
-    getNumbersSuccess : data => ({
-        type:"GET_NUMBERS_SUCCESS",
-        payload: {data}
-    }),
-    getNumbersFailure : error =>({
-        type:"GET_NUMBERS_FAILURE",
-        payload:{ error }
-    })
-}
+  getNumbersRequest: (page, amount) => ({
+    type: "GET_NUMBERS_REQUEST",
+    payload: { page, amount }
+  }),
+  getNumbersSuccess: ({ data, totalPages }) => ({
+    type: "GET_NUMBERS_SUCCESS",
+    payload: { data, totalPages }
+  }),
+  getNumbersFailure: error => ({
+    type: "GET_NUMBERS_FAILURE",
+    payload: { error }
+  })
+};
