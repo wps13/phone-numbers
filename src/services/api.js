6f651cf5995df5832f2a138306573c712cbfb919
Willane Paiva <willanepaiva@hotmail.com>
2019-12-23 12:52:14 -0300

    add new settings to api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:8080/"
-})
+  baseURL: "http://localhost:8080/",
+  crossdomain: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Authorization",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    "Content-Type": "application/json;charset=UTF-8"
+  }
+});
 
-export default api
+export default api;
